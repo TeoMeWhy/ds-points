86ace9c90e615c2d08370e10e3cfd6c96c15ade8
Téo Calvo <teocalvo2@gmail.colm>
2024-06-03 11:10:27 -0300

    Primeiro dia de curso

--- data/query.sql
+++ data/query.sql
@@ -1,1 +1,9 @@
-SELECT * FROM customers
+SELECT *
+
+FROM customers AS t1
+
+LEFT JOIN transactions AS t2
+ON t1.idCustomer = t2.idCustomer
+
+LEFT JOIN transactions_product AS t3
+ON t2.idTransaction = t3.idTransaction
