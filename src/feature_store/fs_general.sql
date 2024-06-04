8ff50c8ea46ab8402e554a3810ab53b0b480128e
Téo Calvo <teocalvo2@gmail.colm>
2024-06-04 09:55:01 -0300

    Feature Store general

--- src/feature_store/fs_general.sql
+++ src/feature_store/fs_general.sql
@@ -1,14 +1,13 @@
 WITH tb_rfv AS (
 
     SELECT 
-
         idCustomer,
-
+        
         CAST(min(julianday('2024-06-04') - julianday(dtTransaction))
             AS INTEGER) + 1 AS recenciaDias,
-
+        
         COUNT(DISTINCT DATE(dtTransaction)) AS frequenciaDias,
-
+        
         SUM(CASE
                 WHEN pointsTransaction > 0 THEN pointsTransaction
             END) AS valorPoints
@@ -19,9 +18,34 @@ WITH tb_rfv AS (
     AND dtTransaction >= DATE('2024-06-04', '-21 day')
 
     GROUP BY idCustomer
+),
+
+tb_idade AS (
+
+    SELECT
+
+        t1.idCustomer,
+
+        CAST(MAX(julianday('2024-06-04') - julianday(t2.dtTransaction))
+                AS INTEGER) + 1 AS idadeBaseDias
+
+    FROM tb_rfv AS t1
+
+    LEFT JOIN transactions AS t2
+    ON t1.idCustomer = t2.idCustomer
+
+    GROUP BY t2.idCustomer
 
 )
 
-SELECT *
+SELECT t1.*,
+     t2.idadeBaseDias,
+     t3.flEmail
 
-FROM tb_rfv
+FROM tb_rfv AS t1
+
+LEFT JOIN tb_idade AS t2
+ON t1.idCustomer = t2.idCustomer
+
+LEFT JOIN customers AS t3
+ON t1.idCustomer = t3.idCustomer
